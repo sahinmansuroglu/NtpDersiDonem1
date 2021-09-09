6d62fefc2c2771335eb26e25dd74b21d8524abe2
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-09-09 10:46:40 +0300

    Update 1.hafta.cs

--- 1.hafta.cs
+++ 1.hafta.cs
@@ -1,5 +1,7 @@
 // ad, soyad şeklinde 2 adet değişken tanımlayıp değerlerini atayınız.
 // Bu iki değişkenin değerini tek bir console.write line ile başına Merhaba kelimesi gelecek şekilde yazınız
+
+//Açıklama: var ile değişken tanımlarken değişkenin tipi ilk atanan değere göre ayarlanır. 
 using System;
 namespace hafta1
 {
