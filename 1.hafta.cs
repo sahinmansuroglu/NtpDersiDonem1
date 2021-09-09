62d434278a87a4d9b1b44e9758b6fff6f3c36c35
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-09-09 10:10:28 +0300

    Update 1.hafta.cs

--- 1.hafta.cs
+++ 1.hafta.cs
@@ -1,2 +1,19 @@
-// Ad soyad şeklinde 2 adet değişken tanımlayıp değerlerini atayınız.
+// ad, soyad şeklinde 2 adet değişken tanımlayıp değerlerini atayınız.
 // Bu iki değişkenin değerini tek bir console.write line ile başına Merhaba kelimesi gelecek şekilde yazınız
+using System;
+namespace hafta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ad = "Şahin";
+            var soyad = "MANSUROĞLU";
+            var yas = 34;
+            var mesaj1 = "Merhaba " + ad + " " + soyad ;
+            var mesaj2 = $"Merhaba {ad} {soyad}";
+            Console.WriteLine(mesaj1);
+            Console.WriteLine(mesaj2);
+        }
+    }
+}
