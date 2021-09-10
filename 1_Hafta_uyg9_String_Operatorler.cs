c739656dc25a4ed42b2e8f7dc005d36a88aa8dd4
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-09-10 13:24:11 +0300

    Update 1_Hafta_uyg9_String_Operatorler.cs

--- 1_Hafta_uyg9_String_Operatorler.cs
+++ 1_Hafta_uyg9_String_Operatorler.cs
@@ -1,1 +1,28 @@
+// Replace ile cümleninin herhangi bir bölümünü değiştirme
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          
+            string orjinalMesaj = "Bugün Hava Çok Güzel";
+
+            //orjinal mesajda "Güzel" kelimesi "Kötü" kelimesi ile değiştirilmiştir
+            string yeniMesaj = orjinalMesaj.Replace("Güzel", "Kötü");
+
+            Console.WriteLine($"orjinal mesaj:{orjinalMesaj}");
+
+            Console.WriteLine($"yeni mesaj:{yeniMesaj}");
+
+
+
+
+
+        }
+    }
+}
 
